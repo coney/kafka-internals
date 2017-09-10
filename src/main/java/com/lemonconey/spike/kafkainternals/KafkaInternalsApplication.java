1888315fceff5639ffc01a32f7f842840ee073d4
Coney Wu <coney.wu@gmail.com>
2017-09-10 23:15:06 +0800

    initial commit

---
+++ src/main/java/com/lemonconey/spike/kafkainternals/KafkaInternalsApplication.java
@@ -0,0 +1,12 @@
+package com.lemonconey.spike.kafkainternals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaInternalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaInternalsApplication.class, args);
+	}
+}
